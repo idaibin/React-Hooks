8931ddc6abd99bb8727f10eeb166f3f20214bf6d
daibin91 <daibin91@163.com>
2019-05-05 17:11:15 +0800

    代码优化

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import Form from "./components/form/index.jsx";
 import Timer from "./components/timer/index.jsx";
 
 import "./styles.css";
 
-function App () {
+function App() {
   return (
     <div className="App">
       <h1>Forms with React Hooks</h1>
