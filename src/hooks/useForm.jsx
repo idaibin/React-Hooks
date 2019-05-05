5627347eb85400346f984b5b1dce5aff217ec042
daibin91 <daibin91@163.com>
2019-05-05 17:29:17 +0800

    取消useCallback

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useState } from "react";
 
 /**
  *
@@ -6,24 +6,27 @@ import { useState, useCallback } from "react";
  * @param {*object} rule 校验规则，正则 和 callback(必须return)
  */
 const useForm = (form = {}, rule = {}) => {
-  const [formData, setFormData] = useState(form)
-  const func = useCallback((event) => {
-    const name = event.target.name
-    let value = event.target.value
+  const [formData, setFormData] = useState(form);
+  const func = event => {
+    const name = event.target.name;
+    let value = event.target.value;
 
-    if (rule[name]) { // 是否存在校验
-      if (typeof rule[name] === 'function') { // callback
-        value = rule[name](value)
-      } else if (!rule[name].test(value)) { // reg
-        value = formData[name]
+    if (rule[name]) {
+      // 是否存在校验
+      if (typeof rule[name] === "function") {
+        // callback
+        value = rule[name](value);
+      } else if (!rule[name].test(value)) {
+        // reg
+        value = formData[name];
       }
     }
     setFormData({
       ...formData,
       [name]: value
-    })
-  })
-  return [formData, func]
-}
+    });
+  };
+  return [formData, func];
+};
 
-export default useForm
+export default useForm;
